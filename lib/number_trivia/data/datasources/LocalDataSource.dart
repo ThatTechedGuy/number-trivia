fa6e87b259dac6d8a171ef0ff413e3a075781edf
ThatTechedGuy <vgupta463@gmail.com>
2020-04-28 16:01:26 +0530

    local data source, fetching last value successful

--- lib/number_trivia/data/datasources/LocalDataSource.dart
+++ lib/number_trivia/data/datasources/LocalDataSource.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
+
 import 'package:numbertrivia/core/error/exceptions.dart';
 import 'package:numbertrivia/number_trivia/data/models/NumberTriviaModel.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-abstract class NumberTriviaLocalDataSource{
-
+abstract class NumberTriviaLocalDataSource {
   // Methods are asynchronous even though some data sources may act in sync with the code.
   // In order to prevent tangent in the intricacies of third party clashes with the application code.
 
@@ -13,5 +15,28 @@ abstract class NumberTriviaLocalDataSource{
   /// Throws [CacheException] if no cached data is present.
 
   Future<NumberTriviaModel> getLastNumberTrivia();
+
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl(this.sharedPreferences);
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final JSONString = sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
+    if (JSONString != null)
+      return Future.value(NumberTriviaModel.fromJSON(jsonDecode(JSONString)));
+    else
+      throw CacheException();
+  }
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    return null;
+  }
 }
---
+++ test/features/number_trivia/data/datasources/LocalDataSource_test.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:numbertrivia/core/error/exceptions.dart';
+import 'package:numbertrivia/number_trivia/data/datasources/LocalDataSource.dart';
+import 'package:numbertrivia/number_trivia/data/models/NumberTriviaModel.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
+
+void main() {
+  MockSharedPreferences mockSharedPreferences;
+  NumberTriviaLocalDataSourceImpl dataSource;
+
+  setUp(() {
+    mockSharedPreferences = MockSharedPreferences();
+    dataSource = NumberTriviaLocalDataSourceImpl(mockSharedPreferences);
+  });
+
+  group('getLastNumberTrivia', () {
+    final tNumberTriviaModel =
+        NumberTriviaModel.fromJSON(jsonDecode(fixture('triviaCached.json')));
+    test(
+        'should return NumberTriviaModel from SharedPreferences when there is one in the cache',
+        () async {
+      // Arrange
+      when(mockSharedPreferences.getString(any))
+          .thenReturn(fixture('triviaCached.json'));
+      // Act
+      final result = await dataSource.getLastNumberTrivia();
+      // Assert
+      verify(mockSharedPreferences.getString(CACHED_NUMBER_TRIVIA));
+    });
+
+    test(
+        'should return CacheException when there is nothing in the local Storage',
+        () async {
+      // Arrange -> arrangement to return an undefined value
+      when(mockSharedPreferences.getString(any)).thenReturn(null);
+      // Act
+      // holds the function itself
+      final call = dataSource.getLastNumberTrivia;
+      // Assert
+      // A CacheException is expected to be thrown on call.
+      expect(() => call(), throwsA(isA<CacheException>()));
+    });
+  });
+}
